8f70488f13c6894bad83cb4e3c0502c657da1f3c
Sasha <q.dev.null@gmail.com>
2023-09-02 10:52:01 +0200

    Add the assets

--- tests/infer/detection/test_scrfd.py
+++ tests/infer/detection/test_scrfd.py
@@ -1,16 +1,24 @@
 import os
 
+import cv2
+import numpy as np
 import pytest
+
+MODEL = "models/det_10g.onnx"
 
 
 def model_exists():
-    file_to_check = "models/det_10g.onnx"
-    return os.path.exists(file_to_check)
+    return os.path.exists(MODEL)
+
+
+@pytest.fixture
+def image() -> np.ndarray:
+    return cv2.imread("tests/assets/lenna.png")
 
 
 @pytest.mark.skipif(
     not model_exists(),
     reason="File doesn't exists, skipping the test",
 )
-def test_srfd_inferencd():
-    print("HERE")
+def test_srfd_inferencd(image: np.ndarray):
+    print("HERE", image.shape)
