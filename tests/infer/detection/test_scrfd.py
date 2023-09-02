b7af74a26723d1c8c2da251df001e110db861916
Sasha <q.dev.null@gmail.com>
2023-09-02 11:03:35 +0200

    Add the model prediction

--- tests/infer/detection/test_scrfd.py
+++ tests/infer/detection/test_scrfd.py
@@ -4,6 +4,8 @@ import cv2
 import numpy as np
 import pytest
 
+from wasp.infer.detection.scrfd import SCRFD
+
 MODEL = "models/det_10g.onnx"
 
 
@@ -16,9 +18,18 @@ def image() -> np.ndarray:
     return cv2.imread("tests/assets/lenna.png")
 
 
+@pytest.fixture
+def model() -> SCRFD:
+    model = SCRFD(MODEL)
+    model.prepare(0, det_rhesh=0.5, input_size=(512, 512))
+    return model
+
+
 @pytest.mark.skipif(
     not model_exists(),
     reason="File doesn't exists, skipping the test",
 )
-def test_srfd_inferencd(image: np.ndarray):
-    print("HERE", image.shape)
+def test_srfd_inferencd(model: SCRFD, image: np.ndarray):
+    bboxes, kps = model.detect(image, max_num=0)
+    print(bboxes)
+    print(kps)
