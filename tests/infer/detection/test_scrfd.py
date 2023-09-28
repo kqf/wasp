9bb1c8daf94b32520abf6c87735bb670939e71d9
Sasha <q.dev.null@gmail.com>
2023-09-28 12:34:47 +0200

    Fix the data

--- tests/infer/detection/test_scrfd.py
+++ tests/infer/detection/test_scrfd.py
@@ -35,14 +35,6 @@ def model_exists():
 
 
 @pytest.fixture
-def image() -> np.ndarray:
-    return cv2.cvtColor(
-        cv2.imread("tests/assets/lenna.png"),
-        cv2.COLOR_BGR2RGB,
-    )
-
-
-@pytest.fixture
 def model() -> SCRFD:
     return SCRFD(MODEL)
 
