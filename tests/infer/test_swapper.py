8a8d78ab7b968cc1692e36171e431ae6aa9c4450
kqf <q.dev.null@gmail.com>
2023-09-29 14:32:42 +0200

    Add face fixtures (#77)
    * Add the conftest
    
    * Fix the data
    
    * Simplify the tests
    
    * Add source
    
    * add the target

--- tests/infer/test_swapper.py
+++ tests/infer/test_swapper.py
@@ -3,10 +3,8 @@ import os
 import numpy as np
 import pytest
 
+from wasp.face import Face
 from wasp.infer.swapper import INSwapper
-
-target = np.zeros((512,))
-source = np.ones((512,))
 
 
 @pytest.fixture
@@ -19,6 +17,46 @@ MODEL = "models/inswapper_128.onnx"
 
 def model_exists():
     return os.path.exists(MODEL)
+
+
+@pytest.fixture
+def keypoints():
+    return np.array(
+        [
+            [269.48657, 267.8749],
+            [334.58054, 266.53412],
+            [313.75803, 321.2535],
+            [271.43204, 349.04657],
+            [315.51984, 347.4897],
+        ]
+    )
+
+
+@pytest.fixture
+def bbox():
+    return np.array(
+        [207.30132, 178.63065, 353.27258, 389.34088],
+    )
+
+
+@pytest.fixture
+def source(bbox, keypoints):
+    return Face(
+        bbox=bbox,
+        kps=keypoints,
+        detection_score=0.9,
+        embedding=np.random.rand(512),
+    )
+
+
+@pytest.fixture
+def target(bbox, keypoints):
+    return Face(
+        bbox=bbox,
+        kps=keypoints,
+        detection_score=0.9,
+        embedding=np.random.rand(512),
+    )
 
 
 @pytest.mark.skipif(
