e725a9c607203aed83d49169f465035f2c881d5c
kqf <q.dev.null@gmail.com>
2023-09-30 10:27:18 +0200

    Flawed assertions (#79)
    * Add the conftest
    
    * Fix the data
    
    * Simplify the tests
    
    * Add source
    
    * add the target
    
    * Add the resize step
    
    * Remove the flawed assertions

--- tests/infer/test_swapper.py
+++ tests/infer/test_swapper.py
@@ -65,8 +65,7 @@ def target(bbox, keypoints):
     reason="File doesn't exists, skipping the test",
 )
 def test_get(swapper, image, target, source):
-    resized_img = cv2.resize(image, (112, 112))
-    result = swapper.get(resized_img, target, source, paste_back=True)
+    result = swapper.get(image, target, source, paste_back=True)
 
     # Assert
     assert isinstance(result, tuple)
