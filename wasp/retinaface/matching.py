9d901c79ab037fbc94479313e1da85da3bb31118
Sasha <q.dev.null@gmail.com>
2023-10-19 08:45:52 +0100

    Add the IoU thing

--- wasp/retinaface/matching.py
+++ wasp/retinaface/matching.py
@@ -29,8 +29,32 @@ def intersect(box_a: torch.Tensor, box_b: torch.Tensor) -> torch.Tensor:
     return inter[:, :, 0] * inter[:, :, 1]
 
 
-def iou(*args):
-    pass
+def iou(box_a: torch.Tensor, box_b: torch.Tensor) -> torch.Tensor:
+    """Computes the jaccard overlap of two sets of boxes.
+
+    The jaccard overlap is simply the intersection over union of two boxes.
+    Here we operate on ground truth boxes and default boxes.
+    E.g.:
+        A ∩ B / A ∪ B = A ∩ B / (area(A) + area(B) - A ∩ B)
+    Args:
+        box_a: Ground truth bounding boxes, Shape: [num_objects,4]
+        box_b: Prior boxes from priorbox layers, Shape: [num_priors,4]
+    Return:
+        jaccard overlap: Shape: [box_a.size(0), box_b.size(0)]
+    """
+    inter = intersect(box_a, box_b)
+    area_a = (
+        ((box_a[:, 2] - box_a[:, 0]) * (box_a[:, 3] - box_a[:, 1]))
+        .unsqueeze(1)
+        .expand_as(inter)
+    )  # [A,B]
+    area_b = (
+        ((box_b[:, 2] - box_b[:, 0]) * (box_b[:, 3] - box_b[:, 1]))
+        .unsqueeze(0)
+        .expand_as(inter)
+    )  # [A,B]
+    union = area_a + area_b - inter
+    return inter / union
 
 
 def point_form(boxes: torch.Tensor) -> torch.Tensor:
