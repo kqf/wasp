84aa1a074de9012746f8e62f933e9657aaa37435
Sasha <q.dev.null@gmail.com>
2023-10-22 14:19:27 +0200

    Add the tests for boxes

--- tests/retinaface/test_matching.py
+++ tests/retinaface/test_matching.py
@@ -34,3 +34,15 @@ def test_match(
         batch_id=batch_id,
     )
     torch.testing.assert_close(label_t, torch.Tensor([[1.0, 0.0]]))
+    print(box_t.cpu().numpy().__repr__())
+    torch.testing.assert_close(
+        box_t,
+        torch.Tensor(
+            [
+                [
+                    [0.000000e00, 0.0000000e00, -6.9314704e00, -9.1629066e00],
+                    [0.000000e00, 6.6227386e-07, -1.3862944e01, -1.5040774e01],
+                ]
+            ]
+        ),
+    )
