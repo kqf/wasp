a3b944749bd4a7ec0201812d479ff5421d14b3f8
kqf <q.dev.null@gmail.com>
2023-10-23 22:56:17 +0200

    Add loss (#135)
    * Add the loss
    
    * Add the boilerplate interface
    
    * Fix the import

--- wasp/retinaface/matching.py
+++ wasp/retinaface/matching.py
@@ -188,3 +188,7 @@ def match(
     box_t[batch_id] = loc  # [num_priors, 4] encoded offsets to learn
     label_t[batch_id] = labels  # [num_priors] top class label for each prior
     landmarks_t[batch_id] = landmarks_gt
+
+
+def log_sum_exp(*args, **kwargs):
+    pass
