7699c9cf3b002fe76016b23ed75748a6c8d62d01
Sasha <q.dev.null@gmail.com>
2023-11-07 07:17:41 +0100

    Fix the fixture name

--- tests/retinaface/test_main.py
+++ tests/retinaface/test_main.py
@@ -1,5 +1,5 @@
 from wasp.retinaface.train import Paths, main
 
 
-def test_main(tmp_dir):
-    main(paths=Paths(tmp_dir, tmp_dir, tmp_dir, tmp_dir))
+def test_main(tmp_path):
+    main(paths=Paths(tmp_path, tmp_path, tmp_path, tmp_path))
