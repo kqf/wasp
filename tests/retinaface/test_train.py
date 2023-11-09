2969bc7c75e9cdf9cb97e549e6559c7fb0e7f6eb
kqf <q.dev.null@gmail.com>
2023-11-09 06:56:02 +0100

    Fix the tests (#176)
    * Update the tests
    
    * Remove argparse
    
    * Switch to configs
    
    * Fix the path to the dict
    
    * Remove the redundant tests
    
    * Simplify the training logic
    
    * Fix the broken fixtures

--- tests/retinaface/test_train.py
+++ tests/retinaface/test_train.py
@@ -1,5 +1,12 @@
-from wasp.retinaface.train import main
+from wasp.retinaface.train import Paths, main
 
 
-def test_main():
-    main()
+def test_main(tmp_path):
+    main(
+        paths=Paths(
+            tmp_path,
+            tmp_path,
+            tmp_path,
+            tmp_path,
+        )
+    )
