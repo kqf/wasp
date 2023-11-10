be491a7dd27720e4b63fc20f87dca17e0379ce87
Sasha <q.dev.null@gmail.com>
2023-11-10 08:14:36 +0100

    Add train

--- tests/retinaface/test_train.py
+++ tests/retinaface/test_train.py
@@ -4,30 +4,29 @@ import pytest
 
 from wasp.retinaface.train import Paths, main
 
-EXAMPLE = [
-    {
-        "file_name": "tests/assets/lenna.png",
-        "annotations": [
-            {
-                "bbox": [449, 330, 571, 720],
-                "landmarks": [
-                    [488.906, 373.643],
-                    [542.089, 376.442],
-                    [515.031, 412.83],
-                    [485.174, 425.893],
-                    [538.357, 431.491],
-                ],
-            }
-        ],
-    },
-]
-
 
 @pytest.fixture
 def annotations(tmp_path):
-    ofile = tmp_path / annotations.json
+    example = [
+        {
+            "file_name": "tests/assets/lenna.png",
+            "annotations": [
+                {
+                    "bbox": [449, 330, 571, 720],
+                    "landmarks": [
+                        [488.906, 373.643],
+                        [542.089, 376.442],
+                        [515.031, 412.83],
+                        [485.174, 425.893],
+                        [538.357, 431.491],
+                    ],
+                }
+            ],
+        },
+    ]
+    ofile = tmp_path / "annotations.json"
     with open(ofile, "w") as f:
-        json.dump(EXAMPLE, f, indent=2)
+        json.dump(example, f, indent=2)
     return ofile
 
 
