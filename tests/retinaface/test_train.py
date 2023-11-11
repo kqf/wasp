1042a19c97db112072197afa9a43959635db7baf
kqf <q.dev.null@gmail.com>
2023-11-11 07:55:49 +0100

    Add dataset fixture (#181)
    * Add the annotaitons file
    
    * Add train
    
    * Use the fixture

--- tests/retinaface/test_train.py
+++ tests/retinaface/test_train.py
@@ -1,12 +1,42 @@
+import json
+import pathlib
+
+import pytest
+
 from wasp.retinaface.train import Paths, main
 
 
-def test_main(tmp_path):
+@pytest.fixture
+def annotations(tmp_path) -> pathlib.Path:
+    example = [
+        {
+            "file_name": "tests/assets/lenna.png",
+            "annotations": [
+                {
+                    "bbox": [449, 330, 571, 720],
+                    "landmarks": [
+                        [488.906, 373.643],
+                        [542.089, 376.442],
+                        [515.031, 412.83],
+                        [485.174, 425.893],
+                        [538.357, 431.491],
+                    ],
+                }
+            ],
+        },
+    ]
+    ofile = tmp_path / "annotations.json"
+    with open(ofile, "w") as f:
+        json.dump(example, f, indent=2)
+    return ofile
+
+
+def test_main(annotations):
     main(
         paths=Paths(
-            tmp_path,
-            tmp_path,
-            tmp_path,
-            tmp_path,
+            annotations,
+            annotations,
+            annotations,
+            annotations,
         )
     )
