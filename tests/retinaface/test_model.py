448d66424036e1fed766384334845b8d510e44fb
Sasha <q.dev.null@gmail.com>
2023-11-28 08:38:04 +0100

    Fix the tests

--- tests/retinaface/test_model.py
+++ tests/retinaface/test_model.py
@@ -27,6 +27,7 @@ def check_shapes(model, image):
     [
         ("resnet50", {"layer2": 1, "layer3": 2, "layer4": 3}, 256),
         ("resnet18", {"layer2": 1, "layer3": 2, "layer4": 3}, 64),
+        ("mobilenet_v2", {"6": 1, "13": 2, "16": 3}, 16),
     ],
 )
 def test_retinaface(inputs, anchors, name, return_layers, in_channels):
@@ -65,12 +66,34 @@ def test_backbone(image):
     # sourcery skip: no-loop-in-tests
     for k, v in poutput.items():
         print(k, v.shape)
+    # Resnet
     # 1 torch.Size([1, 512, 80, 60])
     # 2 torch.Size([1, 1024, 40, 30])
     # 3 torch.Size([1, 2048, 20, 15])
 
+    # Mobilenet_v2
+    # 0 torch.Size([1, 32, 320, 240])
+    # 1 torch.Size([1, 16, 320, 240])
+    # 2 torch.Size([1, 24, 160, 120])
+    # 3 torch.Size([1, 24, 160, 120])
+    # 4 torch.Size([1, 32, 80, 60])
+    # 5 torch.Size([1, 32, 80, 60])
+    # 6 torch.Size([1, 32, 80, 60]) <<<<
+    # 7 torch.Size([1, 64, 40, 30])
+    # 8 torch.Size([1, 64, 40, 30])
+    # 9 torch.Size([1, 64, 40, 30])
+    # 10 torch.Size([1, 64, 40, 30])
+    # 11 torch.Size([1, 96, 40, 30])
+    # 12 torch.Size([1, 96, 40, 30])
+    # 13 torch.Size([1, 96, 40, 30]) <<<<
+    # 14 torch.Size([1, 160, 20, 15])
+    # 15 torch.Size([1, 160, 20, 15])
+    # 16 torch.Size([1, 160, 20, 15]) <<<<
+
+    # Mobilenet
+
     print("---------------------------------")
     model2 = torchvision.models.mobilenet_v2(weights=None)
-    test_outputs = check_shapes(model2, image)
+    test_outputs = check_shapes(model2.features, image)
     for k, v in test_outputs.items():
         print(k, v.shape)
