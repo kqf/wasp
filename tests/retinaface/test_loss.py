aff88bd8502bf90aab6c12fb8fbbe890473f9def
Sasha <q.dev.null@gmail.com>
2023-12-02 07:59:38 +0100

    Fix the dummy loss

--- tests/retinaface/test_loss.py
+++ tests/retinaface/test_loss.py
@@ -1,10 +1,21 @@
 import pytest
 
 from wasp.retinaface.loss import MultiBoxLoss
+from wasp.retinaface.priors import priorbox
 
 
 @pytest.fixture
-def loss(priors):
+def anchors(resolution=(256, 256)):
+    return priorbox(
+        min_sizes=[[16, 32], [64, 128], [256, 512]],
+        steps=[8, 16, 32],
+        clip=False,
+        image_size=resolution,
+    )
+
+
+@pytest.fixture
+def loss(anchors):
     return MultiBoxLoss(
         num_classes=2,
         overlap_thresh=0.35,
@@ -14,7 +25,7 @@ def loss(priors):
         neg_pos=7,
         neg_overlap=0.35,
         encode_target=False,
-        priors=priors,
+        priors=anchors,
         weights={
             "localization": 2,
             "classification": 1,
