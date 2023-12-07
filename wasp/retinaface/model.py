bf661c756ab5e3692bb357dba2cd882b375a3520
kqf <q.dev.null@gmail.com>
2023-12-07 15:15:03 +0100

    Use the last fpn layer (#246)
    * Fix the loss size
    
    * Check the model sizes
    
    * Adjust the tests
    
    * Fix the data
    
    * Limit the number of predictions
    
    * Use only the last FPN layer

--- wasp/retinaface/model.py
+++ wasp/retinaface/model.py
@@ -141,13 +141,13 @@ class RetinaFace(nn.Module):
         fpn = self.fpn(out)
 
         # SSH
-        feature1 = self.ssh1(fpn[0])
-        feature2 = self.ssh2(fpn[1])
-        # feature3 = self.ssh3(fpn[2])
+        # feature1 = self.ssh1(fpn[0])
+        # feature2 = self.ssh2(fpn[1])
+        feature3 = self.ssh3(fpn[2])
         features = [
-            feature1,
-            feature2,
-            # feature3,
+            # feature1,
+            # feature2,
+            feature3,
         ]
 
         bbox_regressions = torch.cat(
