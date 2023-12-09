935b108f98dc16a0ec93bc2648b3facdd980957b
Sasha <q.dev.null@gmail.com>
2023-12-09 19:59:26 +0100

    Remove the duplicate code

--- tests/retinaface/test_priorbox.py
+++ tests/retinaface/test_priorbox.py
@@ -25,17 +25,3 @@ def test_anchors(min_sizes, steps, resolution=RESOLUTION):
     shapes = anchors[:, 2:] * torch.Tensor([resolution])
     shapes = list(map(tuple, shapes.tolist()))
     print(set(shapes))
-
-
-def test_anchors_simplified(resolution=RESOLUTION):
-    """Check no small anchors"""
-    anchors = priorbox(
-        min_sizes=[[64, 128], [256, 512]],
-        steps=[16, 32],
-        clip=False,
-        image_size=resolution,
-    )
-    print(anchors.shape)
-    shapes = anchors[:, 2:] * torch.Tensor([resolution])
-    shapes = list(map(tuple, shapes.tolist()))
-    print(set(shapes))
