219a6a4a7ec21f2b03ae00c40318f5b3da9c220c
Sasha <q.dev.null@gmail.com>
2023-12-19 17:03:26 +0100

    Simplify the script

--- wasp/retinaface/data.mk
+++ wasp/retinaface/data.mk
@@ -1,21 +1,23 @@
 all: wider/train.json wider/valid.json wider/test.json
+images: WIDER_train.zip
+labels: retinaface_gt_v1.1.zip
 
 wider/%.json: wider-labels
-	python wasp/retinaface/convert.py \
+	python /convert.py \
 		--dataset wider-labels/$*/label.txt \
 		--ofile $@
 
-wider-labels: retinaface_gt_v1.1.zip
+wider-labels: $(labels)
 	mkdir -p $@
 	unzip $^ -d wider-labels
 	mv wider-labels/val wider-labels/valid
 
-retinaface_gt_v1.1.zip:
+$(labels):
 	gdown 1BbXxIiY-F74SumCNG6iwmJJ5K3heoemT
 
-wider: WIDER_train.zip
+wider: $(images)
 	unzip $^
 	mv WIDER_train $@
 
-WIDER_train.zip:
+$(images):
 	gdown 15hGDLhsx8bLgLcIRD5DhYt5iBxnjNF1M
