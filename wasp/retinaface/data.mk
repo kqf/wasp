52372aa61853bdf7bcd0a96ad8c4020858ce8e95
kqf <q.dev.null@gmail.com>
2023-12-19 17:04:07 +0100

    Add the data scripts (#271)
    * Add the conversion script
    
    * Simplify the script
    
    * Fix the conversion script path

---
+++ wasp/retinaface/data.mk
@@ -0,0 +1,23 @@
+all: wider/train.json wider/valid.json wider/test.json
+images: WIDER_train.zip
+labels: retinaface_gt_v1.1.zip
+
+wider/%.json: wider-labels
+	python wasp/retinaface/convert.py \
+		--dataset wider-labels/$*/label.txt \
+		--ofile $@
+
+wider-labels: $(labels)
+	mkdir -p $@
+	unzip $^ -d wider-labels
+	mv wider-labels/val wider-labels/valid
+
+$(labels):
+	gdown 1BbXxIiY-F74SumCNG6iwmJJ5K3heoemT
+
+wider: $(images)
+	unzip $^
+	mv WIDER_train $@
+
+$(images):
+	gdown 15hGDLhsx8bLgLcIRD5DhYt5iBxnjNF1M
