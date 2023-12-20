7176d62b64417cbe5dc26bb0a3b85c36000960ef
Sasha <q.dev.null@gmail.com>
2023-12-20 16:04:14 +0100

    Promote to a function

--- wasp/retinaface/preprocess.py
+++ wasp/retinaface/preprocess.py
@@ -128,38 +128,34 @@ def flip_landmark(landms, arg1, arg2):
     landms[:, arg2, :] = tmp
 
 
-class Preproc:
-    def __init__(self, img_dim: int) -> None:
-        self.img_dim = img_dim
+def Preproc(
+    img_dim: int,
+    image: np.ndarray,
+    targets: np.ndarray,
+) -> tuple[np.ndarray, np.ndarray]:
+    if targets.shape[0] == 0:
+        raise ValueError("this image does not have gt")
 
-    def __call__(
-        self,
-        image: np.ndarray,
-        targets: np.ndarray,
-    ) -> tuple[np.ndarray, np.ndarray]:
-        if targets.shape[0] == 0:
-            raise ValueError("this image does not have gt")
+    boxes = targets[:, :4].copy()
+    landmarks = targets[:, 4:-1].copy()
+    labels = targets[:, -1:].copy()
 
-        boxes = targets[:, :4].copy()
-        landmarks = targets[:, 4:-1].copy()
-        labels = targets[:, -1:].copy()
+    image_t, boxes_t, labels_t, landmarks_t, pad_image_flag = random_crop(
+        image, boxes, labels, landmarks, img_dim
+    )
 
-        image_t, boxes_t, labels_t, landmarks_t, pad_image_flag = random_crop(
-            image, boxes, labels, landmarks, self.img_dim
-        )
+    image_t = _pad_to_square(image_t, pad_image_flag)
+    image_t, boxes_t, landmarks_t = random_horizontal_flip(
+        image_t, boxes_t, landmarks_t
+    )
+    height, width = image_t.shape[:2]
 
-        image_t = _pad_to_square(image_t, pad_image_flag)
-        image_t, boxes_t, landmarks_t = random_horizontal_flip(
-            image_t, boxes_t, landmarks_t
-        )
-        height, width = image_t.shape[:2]
+    boxes_t[:, 0::2] = boxes_t[:, 0::2] / width
+    boxes_t[:, 1::2] = boxes_t[:, 1::2] / height
 
-        boxes_t[:, 0::2] = boxes_t[:, 0::2] / width
-        boxes_t[:, 1::2] = boxes_t[:, 1::2] / height
+    landmarks_t[:, 0::2] = landmarks_t[:, 0::2] / width
+    landmarks_t[:, 1::2] = landmarks_t[:, 1::2] / height
 
-        landmarks_t[:, 0::2] = landmarks_t[:, 0::2] / width
-        landmarks_t[:, 1::2] = landmarks_t[:, 1::2] / height
+    targets_t = np.hstack((boxes_t, landmarks_t, labels_t))
 
-        targets_t = np.hstack((boxes_t, landmarks_t, labels_t))
-
-        return image_t, targets_t
+    return image_t, targets_t
