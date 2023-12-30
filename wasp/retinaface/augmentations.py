7575f196f71d677c3abc9b631d4d30f977dd6fff
kqf <q.dev.null@gmail.com>
2023-12-30 08:36:00 +0100

    Add validation augmentations (#297)
    * Add the train augmentations
    
    * Use augs
    
    * Fix the yaml code
    
    * Fix the broken pipeline
    
    * Add the validation augmentations

--- wasp/retinaface/augmentations.py
+++ wasp/retinaface/augmentations.py
@@ -8,7 +8,7 @@ from albumentations import (
 )
 
 
-def train():
+def train() -> Compose:
     return Compose(
         bbox_params=None,
         keypoint_params=None,
@@ -22,6 +22,24 @@ def train():
             ),
             HueSaturationValue(hue_shift_limit=20, val_shift_limit=20, p=0.5),
             RandomGamma(gamma_limit=[80, 120], p=0.5),
+            Resize(height=256, width=256, p=1),
+            Normalize(
+                always_apply=False,
+                max_pixel_value=255.0,
+                mean=[0.485, 0.456, 0.406],
+                p=1,
+                std=[0.229, 0.224, 0.225],
+            ),
+        ],
+    )
+
+
+def valid() -> Compose:
+    return Compose(
+        bbox_params=None,
+        keypoint_params=None,
+        p=1,
+        transforms=[
             Resize(height=256, width=256, p=1),
             Normalize(
                 always_apply=False,
