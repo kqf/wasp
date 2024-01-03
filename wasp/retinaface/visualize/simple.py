9b9e60fbe8548b22509c32ddba4c49d5d217c693
kqf <q.dev.null@gmail.com>
2024-01-03 10:00:26 +0100

    Move common parts (#306)
    * Fix the broken scripts
    
    * Move the common parts

--- wasp/retinaface/visualize/simple.py
+++ wasp/retinaface/visualize/simple.py
@@ -3,57 +3,9 @@ import pathlib
 import click
 import cv2
 import matplotlib.pyplot as plt
-import numpy as np
-from environs import Env
 
-from wasp.retinaface.data import Annotation, read_dataset
-
-env = Env()
-env.read_env()
-
-
-def to_local(filename):
-    return filename.replace(env.str("PRIVATE_STORAGE_LOCATION"), "")
-
-
-def plot(
-    image: np.ndarray,
-    annotations: list[Annotation],
-) -> np.ndarray:
-    vis_image = image.copy()
-    print(annotations)
-
-    for annotation in annotations:
-        colors = [
-            (255, 0, 0),
-            (128, 255, 0),
-            (255, 178, 102),
-            (102, 128, 255),
-            (0, 255, 255),
-        ]
-
-        for landmark_id, (x, y) in enumerate(annotation.landmarks):
-            vis_image = cv2.circle(
-                vis_image,
-                (int(x), int(y)),
-                radius=3,
-                color=colors[landmark_id],
-                thickness=3,
-            )
-
-        x_min, y_min, x_max, y_max = (int(tx) for tx in annotation.bbox)
-        x_min = np.clip(x_min, 0, x_max - 1)
-        y_min = np.clip(y_min, 0, y_max - 1)
-
-        vis_image = cv2.rectangle(
-            vis_image,
-            (x_min, y_min),
-            (x_max, y_max),
-            color=(0, 255, 0),
-            thickness=2,
-        )
-
-    return vis_image
+from wasp.retinaface.data import read_dataset
+from wasp.retinaface.visualize.plot import plot, to_local
 
 
 @click.command()
