bd75ad915e44bc8edfdef87acce183848b3c7c98
Sasha <q.dev.null@gmail.com>
2024-01-07 09:07:56 +0100

    Fix the transformed plotting

--- wasp/retinaface/visualize/augmentations.py
+++ wasp/retinaface/visualize/augmentations.py
@@ -3,9 +3,10 @@ import pathlib
 import click
 import cv2
 import matplotlib.pyplot as plt
+import numpy as np
 
 from wasp.retinaface.augmentations import train
-from wasp.retinaface.data import read_dataset
+from wasp.retinaface.data import Annotation, read_dataset
 from wasp.retinaface.visualize.plot import plot, to_local
 
 
@@ -16,17 +17,25 @@ from wasp.retinaface.visualize.plot import plot, to_local
         exists=True,
         path_type=pathlib.Path,
     ),
-    default="wider/test.json",
+    default="wider/train.json",
 )
 def main(dataset):
     labels = read_dataset(dataset)
     for sample in labels:
         image = cv2.imread(to_local(sample.file_name))
         transofrm = train()
-        sample = transofrm(image=image, keypoints=sample.annotations)
+        boxes, keypoints = sample.flatten()
+        sample = transofrm(
+            image=image,
+            bbox=np.asarray(boxes),
+            keypoints=np.asarray(keypoints),
+        )
         image = sample["image"]
-        annotations = sample["annotations"]
-        plt.imshow(plot(image, annotations=annotations))
+        boxes = sample["bbox"]
+        keypoints = sample["keypoints"]
+        transformed = [Annotation(b, k) for b, k in zip(boxes, keypoints)]
+
+        plt.imshow(plot(image, annotations=transformed))
         plt.show()
 
 
