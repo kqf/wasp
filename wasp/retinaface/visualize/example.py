d40c10202acd24bbdba1f6fab9302d5cbce48fbf
kqf <q.dev.null@gmail.com>
2024-01-20 11:47:40 +0000

    Check random keypoints (#344)
    * Use just dicts
    
    * Disable the resize
    
    * Add resize
    
    * Turn back the resizes
    
    * Add the multiple keypoints
    
    * Make the augmentations work
    
    * Fix the ignore boxes
    
    * Add back the normalization
    
    * Reduce back the image size
    
    * Fix the images
    
    * Fix the augmentations
    
    * Use back the transformed keypoints
    
    * Fix the categories
    
    * Add the basic example
    
    * Fix the examples
    
    * Fix the couple
    
    * Fix an example
    
    * Remove the invocation parameters
    
    * Remove samples
    
    * Remove the auxiliary methods
    
    * Simplify the transforms
    
    * Add an ill example
    
    * Remove the augmentaitons
    
    * Remove the redundant function
    
    * Add the random keypoints

--- wasp/retinaface/visualize/example.py
+++ wasp/retinaface/visualize/example.py
@@ -110,71 +110,17 @@ def works():
         image=image,
         bboxes=boxes,
         category_ids=np.ones(len(boxes)),
-        keypoints=np.asarray(keypoints).reshape(-1, 2),
+        keypoints=keypoints[0],
+        keypoints1111=keypoints[1],
     )
 
     image = sample["image"]
     boxes = sample["bboxes"]
     transofrmed_keypoints = np.asarray(sample["keypoints"])
-    keypoints = transofrmed_keypoints.reshape(-1, 5, 2)
-    transformed = [Annotation(b, k) for b, k in zip(boxes, keypoints)]
-
-    plt.imshow(plot(image, annotations=transformed))
-    plt.show()
-
-
-def main():
-    image = cv2.cvtColor(
-        cv2.imread("couple.jpg"),
-        cv2.COLOR_BGR2RGB,
-    )
-
-    boxes = (
-        (332, 128, 542, 424),
-        (542, 232, 726, 498),
-    )
-    keypoints = (
-        [
-            [410.562, 223.625],
-            [482.817, 268.089],
-            [436.5, 286.616],
-            [364.246, 301.438],
-            [443.911, 344.049],
-        ],
-        [
-            [590.205, 329.531],
-            [676.795, 337.857],
-            [633.5, 381.152],
-            [580.214, 417.786],
-            [668.469, 429.442],
-        ],
-    )
-
-    transform = alb.Compose(
-        bbox_params=alb.BboxParams(
-            format="pascal_voc",
-            label_fields=["category_ids"],
-        ),
-        keypoint_params=alb.KeypointParams(
-            format="xy",
-        ),
-        p=1,
-        transforms=[
-            alb.Resize(height=1024, width=1024, p=1),
-        ],
-    )
-
-    sample = transform(
-        image=image,
-        bboxes=boxes,
-        category_ids=np.ones(len(boxes)),
-        keypoints=keypoints,
-    )
-
-    image = sample["image"]
-    boxes = sample["bboxes"]
-    transofrmed_keypoints = np.asarray(sample["keypoints"])
-    keypoints = transofrmed_keypoints.reshape(-1, 5, 2)
+    keypoints = [
+        transofrmed_keypoints,
+        sample["keypoints1111"],
+    ]
     transformed = [Annotation(b, k) for b, k in zip(boxes, keypoints)]
 
     plt.imshow(plot(image, annotations=transformed))
