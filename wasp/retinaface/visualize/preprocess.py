9dd32930be20ea2d46c41a0ae25d4d8a22e91308
kqf <q.dev.null@gmail.com>
2024-01-24 11:23:53 +0000

    Cleanup imports (#355)
    * Fix the augmentaiton resolution
    
    * Fix the augmentations misnomer
    
    * Don't filter the dataset
    
    * Add the preprocessing
    
    * Apply preprocessing
    
    * Fix the broken script
    
    * Convert the masks to annotations
    
    * Fix the pre-commit things
    
    * Remove the redundant imports

--- wasp/retinaface/visualize/preprocess.py
+++ wasp/retinaface/visualize/preprocess.py
@@ -4,7 +4,7 @@ import click
 import cv2
 import numpy as np
 
-from wasp.retinaface.data import AbsoluteXYXY, Sample, read_dataset
+from wasp.retinaface.data import Sample, read_dataset, trimm_boxes
 from wasp.retinaface.preprocess import preprocess
 from wasp.retinaface.visualize.plot import to_local
 
@@ -14,22 +14,7 @@ def with_masks(keypoints):
     mask = keypoints < 0
     return mask, keypoints.clip(0, 1024)
 
-
-def trimm_boxes(
-    bbox: AbsoluteXYXY,
-    image_width: int,
-    image_height: int,
-) -> AbsoluteXYXY:
-    x_min, y_min, x_max, y_max = bbox
-
-    x_min = np.clip(x_min, 0, image_width - 1)
-    y_min = np.clip(y_min, 0, image_height - 1)
-    x_max = np.clip(x_max, x_min + 1, image_width - 1)
-    y_max = np.clip(y_max, y_min, image_height - 1)
-
-    return x_min, y_min, x_max, y_max
-
-
+  
 def to_annotations(sample: Sample, image_width, image_height) -> np.ndarray:
     num_annotations = 4 + 10 + 1
     annotations = np.zeros((0, num_annotations))
