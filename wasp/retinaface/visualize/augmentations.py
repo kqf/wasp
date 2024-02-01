fb175acb517ca32b6ece5d64c90705fa9bd50b54
Sasha <q.dev.null@gmail.com>
2024-02-01 08:28:52 +0000

    Experiment with augmentations

--- wasp/retinaface/visualize/augmentations.py
+++ wasp/retinaface/visualize/augmentations.py
@@ -1,13 +1,43 @@
 import pathlib
 
+import albumentations as alb
 import click
 import cv2
 import matplotlib.pyplot as plt
 import numpy as np
 
-from wasp.retinaface.augmentations import train
 from wasp.retinaface.data import Annotation, read_dataset
 from wasp.retinaface.visualize.plot import plot, to_local
+
+
+def train() -> alb.Compose:
+    return alb.Compose(
+        bbox_params=None,
+        keypoint_params=None,
+        p=1,
+        transforms=[
+            alb.RandomBrightnessContrast(
+                always_apply=False,
+                brightness_limit=0.2,
+                contrast_limit=[0.5, 1.5],
+                p=0.5,
+            ),
+            alb.HueSaturationValue(
+                hue_shift_limit=20,
+                val_shift_limit=20,
+                p=0.5,
+            ),
+            alb.RandomGamma(gamma_limit=[80, 120], p=0.5),
+            alb.Resize(height=256, width=256, p=1),
+            alb.Normalize(
+                always_apply=False,
+                max_pixel_value=255.0,
+                mean=[0.485, 0.456, 0.406],
+                p=1,
+                std=[0.229, 0.224, 0.225],
+            ),
+        ],
+    )
 
 
 def with_masks(keypoints):
