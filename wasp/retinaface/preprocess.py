Fix the normalization (#422)
* Add the meaningful validation

* Add the more meaningful validation

* Don't drop the last channel

* Reduce the number of epochs

* Fix the type mismatch

* Fix the annotation errors

* Simplify the preprcessing

* Add the function composition

* Remove the augmentations

* Fix the wrong imports

* Fix the broken normalization

* Modify the example

* Increase the number of epochs

* Fix the normalization

* Fix the pre-processing test

wasp/retinaface/preprocess.py
@@ -176,10 +176,10 @@ def normalize(
 
     height, width = image.shape[:2]
 
-    boxes[:, 0::2] = boxes[:, 0::2] / height
+    boxes[:, 0::2] = boxes[:, 0::2] / width
     boxes[:, 1::2] = boxes[:, 1::2] / height
 
-    landmarks[:, 0::2] = landmarks[:, 0::2] / height
+    landmarks[:, 0::2] = landmarks[:, 0::2] / width
     landmarks[:, 1::2] = landmarks[:, 1::2] / height
 
     targets_t = np.hstack((boxes, landmarks, labels))