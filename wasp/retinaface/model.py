dcc2db2197c5a64084be720f91b695ddcc38c14f
kqf <q.dev.null@gmail.com>
2024-02-24 09:57:04 +0000

    Fix depths output channels (#433)
    * Fix the data
    
    * Extend the collate function
    
    * Fix the loss
    
    * Fix the pipline
    
    * Switch to dict annotations
    
    * Fix the broken data
    
    * Fix the broken pipeline
    
    * Fix the broken pipelines
    
    * Fix the loss calculation
    
    * Fix the training data
    
    * Add the depths
    
    * Add the depths heads
    
    * Fix the number of output channels

--- wasp/retinaface/model.py
+++ wasp/retinaface/model.py
@@ -64,7 +64,7 @@ class DepthsHead(nn.Module):
         super().__init__()
         self.conv1x1 = nn.Conv2d(
             in_channels,
-            num_anchors * 10,
+            num_anchors * 1,
             kernel_size=(1, 1),
             stride=1,
             padding=0,
@@ -73,8 +73,7 @@ class DepthsHead(nn.Module):
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         out = self.conv1x1(x)
         out = out.permute(0, 2, 3, 1).contiguous()
-        return out.view(out.shape[0], -1, 10)
-
+        return out.view(out.shape[0], -1, 1)
 
 def _make_classes(
     fpn_num: int = 3, in_channels: int = 64, anchor_num: int = 2
