9239d738c3e326ddad3e06a6142368f6d82770c9
Sasha <q.dev.null@gmail.com>
2024-03-12 07:37:37 +0100

    Adjust the API

--- wasp/retinaface/loss.py
+++ wasp/retinaface/loss.py
@@ -49,7 +49,6 @@ class MultiBoxLoss(nn.Module):
         self,
         num_classes: int,
         overlap_thresh: float,
-        neg_mining: bool,
         neg_pos: int,
         neg_overlap: float,
         priors: torch.Tensor,
