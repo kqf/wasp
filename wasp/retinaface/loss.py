0175d38470ccf9477cac4105cc34ab64ab05fe6d
Sasha <q.dev.null@gmail.com>
2024-03-27 06:43:09 +0100

    Fix the positive label definition

--- wasp/retinaface/loss.py
+++ wasp/retinaface/loss.py
@@ -205,8 +205,8 @@ class MultiBoxLoss(nn.Module):
             data=dpths_t[pos_depth].view(-1, 2),
             pred=dpt_data[pos_depth].view(-1, 2),
         )
-        # loss_dpth, ndpth = depths_loss(label_t, dpt_data, dpths_t)
-        positive = label_t != torch.zeros_like(label_t)
+
+        positive = label_t > torch.zeros_like(label_t)
         label_t[positive] = 1
 
         # Localization Loss (Smooth L1) Shape: [batch, num_priors, 4]
