2b3fc338ceb117e655560506b47b646a665ad8b4
kqf <q.dev.null@gmail.com>
2024-05-09 06:53:27 +0200

    Try different selection (#609)
    * Fix the losses one by one
    
    * Fix the losses one by one
    
    * Fix back the pointform
    
    * Add the encode form
    
    * Add the decode function
    
    * Add the landmark decodings corrections
    
    * Add back the empty loss
    
    * Reduce the time comlexity of the tests
    
    * Update the loss
    
    * Rename the n_preds
    
    * Fix the albumentations
    
    * Rename the predictions
    
    * Update the loss
    
    * Increase the negpos
    
    * Increase the depth performance
    
    * Fix back the negpos ratio
    
    * Don't force positives
    
    * Force the classification loss
    
    * Don't do the redundant checks inside the loss
    
    * Unify the positive conditions
    
    * Try a bit different selection

--- wasp/retinaface/loss.py
+++ wasp/retinaface/loss.py
@@ -50,7 +50,7 @@ def depths_loss(
     dpt_pred: torch.Tensor,
     dpths_t: torch.Tensor,
 ) -> tuple[torch.Tensor, torch.Tensor]:
-    positive_depth = label_t > torch.zeros_like(label_t)
+    positive_depth = label_t != torch.zeros_like(label_t)
     pos_depth = positive_depth.unsqueeze(positive_depth.dim()).expand_as(
         dpt_pred,
     )
@@ -82,7 +82,7 @@ def landmark_loss(
     kypts_t: torch.Tensor,
 ) -> tuple[torch.Tensor, torch.Tensor]:
     # landmark Loss (Smooth L1) Shape: [batch, num_priors, 10]
-    positive_1 = label_t > torch.zeros_like(label_t)
+    positive_1 = label_t != torch.zeros_like(label_t)
     # num_positive_landmarks = positive_1.long().sum(1, keepdim=True)
     # n1 = max(num_positive_landmarks.data.sum().float(), 1)  # type: ignore
     pos_idx1 = positive_1.unsqueeze(positive_1.dim()).expand_as(
