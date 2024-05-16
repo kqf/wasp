1ef09cd106d9c62ac0f68f12dc2987908dd84812
Sasha <q.dev.null@gmail.com>
2024-05-16 07:14:17 +0200

    Fix the loss

--- wasp/retinaface/loss.py
+++ wasp/retinaface/loss.py
@@ -50,7 +50,7 @@ def depths_loss(
     dpt_pred: torch.Tensor,
     dpths_t: torch.Tensor,
 ) -> tuple[torch.Tensor, torch.Tensor]:
-    positive_depth = label_t > torch.zeros_like(label_t)
+    positive_depth = label_t != torch.zeros_like(label_t)
     pos_depth = positive_depth.unsqueeze(positive_depth.dim()).expand_as(
         dpt_pred,
     )
@@ -68,7 +68,7 @@ def localization_loss(
     boxes_t: torch.Tensor,
 ) -> tuple[torch.Tensor, torch.Tensor]:
     # Localization Loss (Smooth L1) Shape: [batch, num_priors, 4]
-    positive = label_t > torch.zeros_like(label_t)
+    positive = label_t != torch.zeros_like(label_t)
     pos_idx = positive.unsqueeze(positive.dim()).expand_as(boxes_pred)
     return masked_loss(
         partial(F.smooth_l1_loss, reduction="sum"),
@@ -83,7 +83,7 @@ def landmark_loss(
     kypts_t: torch.Tensor,
 ) -> tuple[torch.Tensor, torch.Tensor]:
     # landmark Loss (Smooth L1) Shape: [batch, num_priors, 10]
-    positive_1 = label_t > torch.zeros_like(label_t)
+    positive_1 = label_t != torch.zeros_like(label_t)
     # num_positive_landmarks = positive_1.long().sum(1, keepdim=True)
     # n1 = max(num_positive_landmarks.data.sum().float(), 1)  # type: ignore
     pos_idx1 = positive_1.unsqueeze(positive_1.dim()).expand_as(
@@ -104,7 +104,7 @@ def confidence_loss(
     negpos_ratio: float,
     num_classes: int,
 ) -> tuple[torch.Tensor, torch.Tensor]:
-    positive = label_t > torch.zeros_like(label_t)
+    positive = label_t != torch.zeros_like(label_t)
     label_t[positive] = 1
     batch_conf = confidence_data.view(-1, num_classes)
     loss_c = log_sum_exp(batch_conf) - batch_conf.gather(
