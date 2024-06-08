71de04a06a6ad66b0c7a1f0fb44f1a7962175684
kqf <q.dev.null@gmail.com>
2024-06-08 05:03:11 +0200

    Add the tensor shape annotations (#681)
    * Simplify further the matching
    
    * Add the tensor shapes annotations

--- wasp/retinaface/matching.py
+++ wasp/retinaface/matching.py
@@ -28,26 +28,30 @@ def iou(box_a: torch.Tensor, box_b: torch.Tensor) -> torch.Tensor:
 
 
 def match(
-    labels: torch.Tensor,
-    boxes: torch.Tensor,
-    priors: torch.Tensor,
+    labels: torch.Tensor,  # [n_obj]
+    boxes: torch.Tensor,  # [n_obj, 4]
+    priors: torch.Tensor,  # [n_anchors, 4]
     threshold: float,
 ) -> tuple[torch.Tensor, torch.Tensor] | tuple[None, None]:
-    overlaps = iou(boxes, point_form(priors))
+    overlaps = iou(boxes, point_form(priors))  # [n_obj, n_anchors]
 
     # (Bipartite Matching)
-    best_prior_overlap, best_prior_idx = overlaps.max(1, keepdim=True)
-    valid_gt_idx = best_prior_overlap[:, 0] >= 0.2
+    best_prior_overlap, best_prior_idx = overlaps.max(
+        1, keepdim=True
+    )  # [n_obj, 1], [n_obj, 1]
+    valid_gt_idx = best_prior_overlap[:, 0] >= 0.2  # [n_obj]
 
     if valid_gt_idx.sum() == 0:
         return None, None
 
-    best_truth_overlap, best_truth_idx = overlaps.max(0, keepdim=True)
-    best_truth_overlap = best_truth_overlap.squeeze(0)
-    best_prior_idx = best_prior_idx.squeeze(1)
+    best_truth_overlap, best_truth_idx = overlaps.max(
+        0, keepdim=True
+    )  # [1, n_anchors], [1, n_anchors]
+    best_truth_overlap = best_truth_overlap.squeeze(0)  # [n_anchors]
+    best_prior_idx = best_prior_idx.squeeze(1)  # [n_obj]
     best_truth_overlap.index_fill_(0, best_prior_idx[valid_gt_idx], 2)
 
-    best_truth_idx = best_truth_idx.squeeze(0)
+    best_truth_idx = best_truth_idx.squeeze(0)  # [n_anchors
 
     # Use arange instead of for loop
     best_truth_idx[best_prior_idx] = torch.arange(
@@ -55,7 +59,7 @@ def match(
         device=best_prior_idx.device,
     )
 
-    labels_matched = labels[best_truth_idx].clone()
+    labels_matched = labels[best_truth_idx].clone()  # [n_anchors]
     labels_matched[best_truth_overlap < threshold] = 0
 
     return best_truth_idx, labels_matched
