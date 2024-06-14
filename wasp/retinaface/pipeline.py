fad0f56cf1d191d8735627f10cbe0243243ff93b
kqf <q.dev.null@gmail.com>
2024-06-14 06:27:25 +0200

    Reduce batch size further (#695)
    * Fix the broken loss function
    
    * Fix the numel
    
    * Stop using legacy constructor
    
    * Try the masked select
    
    * Reduce the batch size
    
    * Try detaching the loss
    
    * Reduce the batch size even further

--- wasp/retinaface/pipeline.py
+++ wasp/retinaface/pipeline.py
@@ -110,7 +110,7 @@ class RetinaFacePipeline(pl.LightningModule):  # pylint: disable=R0901
                 preproc=compose(normalize, self.preproc),
                 rotate90=False,
             ),
-            batch_size=4,
+            batch_size=2,
             num_workers=4,
             shuffle=True,
             pin_memory=True,
