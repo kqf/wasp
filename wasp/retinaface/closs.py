ccf477f8714c0997e9897de06be7dd0ee564bab0
kqf <q.dev.null@gmail.com>
2024-07-08 07:43:24 +0200

    Fix the closses (#753)
    * Start back with a single device
    
    * Fix the gpu number
    
    * Fix the gpus
    
    * Fix the gpus
    
    * Remove the reduction
    
    * Fix the squeezing
    
    * Fix the sum reduction
    
    * Fix the regression
    
    * Don't increase the loss
    
    * Fix the loss
    
    * Fix the class weight
    
    * Parametrize negpos ratio
    
    * Add the missing annotations]
    
    * Add the missing nograd
    
    * Use a single GPU
    
    * Reduce the batch size
    
    * Try to generate the random mask
    
    * Fix the loss typo
    
    * Fix another typo
    
    * Add more annotations
    
    * fix linting
    
    * Fix the device stats monitor
    
    * Fix the closses

--- wasp/retinaface/closs.py
+++ wasp/retinaface/closs.py
@@ -314,8 +314,5 @@ class DetectionLoss(torch.nn.Module):
             )
             losses[name] = subloss(y_pred_, y_true_, anchor_)
 
-        total = torch.sum(losses.values())
-        # detach the losses, from the graph
-        losses = {k: v.detach() for k, v in losses.items()}
-        losses["loss"] = total
-        return total
+        losses["loss"] = torch.stack(tuple(losses.values())).sum()
+        return losses
