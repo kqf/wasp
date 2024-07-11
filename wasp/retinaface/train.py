3412089cf8e5b503ad243802daacbffbc65e1597
Sasha <q.dev.null@gmail.com>
2024-07-11 07:36:31 +0200

    Fix the training

--- wasp/retinaface/train.py
+++ wasp/retinaface/train.py
@@ -81,8 +81,8 @@ def main(
         num_sanity_val_steps=0,
         benchmark=True,
         precision=32,
-        # sync_batchnorm=torch.cuda.is_available(),
-        sync_batchnorm=False,
+        sync_batchnorm=torch.cuda.is_available(),
+        # sync_batchnorm=False,
         logger=build_mlflow(),
         callbacks=[
             BestModelCheckpoint(
