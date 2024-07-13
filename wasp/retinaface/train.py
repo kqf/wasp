5c42ba0f5d7c7bb4700234d55432d824431569e5
Sasha <q.dev.null@gmail.com>
2024-07-13 10:29:39 +0200

    Reduce back the precision

--- wasp/retinaface/train.py
+++ wasp/retinaface/train.py
@@ -80,7 +80,7 @@ def main(
         strategy="ddp_find_unused_parameters_true",
         num_sanity_val_steps=0,
         benchmark=True,
-        precision=32,
+        precision=16,
         sync_batchnorm=torch.cuda.is_available(),
         # sync_batchnorm=False,
         logger=build_mlflow(),
