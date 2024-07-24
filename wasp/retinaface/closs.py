c5133c4593d70bb4f89944ae3c0942070ab01e75
kqf <q.dev.null@gmail.com>
2024-07-24 08:16:33 +0200

    Switch back masking (#791)
    * Fix the monitoring
    
    * Remove the failing callback
    
    * FIx the monitor api
    
    * Fix the monitor
    
    * Simplify the codestyle
    
    * Reuse the logger
    
    * Fix the average stats
    
    * Bump the batch size
    
    * Fix everything
    
    * Don't call the stats
    
    * enable the keypoints
    
    * Fix the encoding issues
    
    * Remove the keypoints
    
    * Try using all devices
    
    * Switch back to the masked functions
    
    * Fix the continuous loss

--- wasp/retinaface/closs.py
+++ wasp/retinaface/closs.py
@@ -166,30 +166,30 @@ def masked_loss(
             0.0, device=data.device, requires_grad=True
         )  # Ensure gradient tracking
 
-    # mask = ~torch.isnan(data)
+    mask = ~torch.isnan(data)
 
-    # try:
-    #     data_masked = data[mask]
-    #     pred_masked = pred[mask]
-    # except RuntimeError as e:
-    #     print(f"===> {pred.shape=}, {data.shape=}, {mask.shape=}")
-    #     raise e
+    try:
+        data_masked = data[mask]
+        pred_masked = pred[mask]
+    except RuntimeError as e:
+        print(f"===> {pred.shape=}, {data.shape=}, {mask.shape=}")
+        raise e
 
     loss = loss_function(
-        pred,
-        data,
+        pred_masked,
+        data_masked,
     )
-    # if data_masked.numel() == 0:
-    #     return torch.tensor(
-    #         0.0, device=data.device, requires_grad=True
-    #     )  # Ensure gradient tracking
+    if data_masked.numel() == 0:
+        return torch.tensor(
+            0.0, device=data.device, requires_grad=True
+        )  # Ensure gradient tracking
 
     # Check for non-finite values and return zero if any are found
-    # if not torch.isfinite(loss).all():
-    #     print(f"Non-finite loss detected: {loss.item()}")
-    #     return torch.tensor(
-    #         0.0, device=data.device, requires_grad=True
-    #     )  # Ensure gradient tracking
+    if not torch.isfinite(loss).all():
+        print(f"Non-finite loss detected: {loss.item()}")
+        return torch.tensor(
+            0.0, device=data.device, requires_grad=True
+        )  # Ensure gradient tracking
 
     return torch.nan_to_num(loss) / max(data.shape[0], 1)
 
