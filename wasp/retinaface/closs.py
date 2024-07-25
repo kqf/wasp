Fix precision issue (#795)
* Start back with a single device

* Fix the gpu number

* Fix the gpus

* Fix the gpus

* Remove the reduction

* Fix the squeezing

* Fix the sum reduction

* Fix the regression

* Don't increase the loss

* Fix the loss

* Fix the class weight

* Parametrize negpos ratio

* Add the missing annotations]

* Add the missing nograd

* Use a single GPU

* Reduce the batch size

* Try to generate the random mask

* Fix the loss typo

* Fix another typo

* Add more annotations

* fix linting

* Fix the device stats monitor

* Fix the closses

* Add the missing annotation

* Fix the depths loss

* Fix the loss

* Fix the losses

* Fix the precision issue

* Remove the sync batchnorm

wasp/retinaface/closs.py
@@ -211,8 +211,8 @@ def default_losses(variance=None):
         #         masked_loss,
         #         loss_function=torch.nn.SmoothL1Loss(),
         #     ),
-        #     enc_true=lambda x, a: encl(x, a, variances=variance),
-        #     # enc_true=encode,
+        #     # enc_true=lambda x, a: encl(x, a, variances=variance),
+        #     enc_true=encode,
         #     weight=1,
         # ),
         # "depths": WeightedLoss(