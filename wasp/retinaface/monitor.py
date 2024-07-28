d5b5846a1b0d4d62ad4afe2f7704ad14cca3b610
kqf <q.dev.null@gmail.com>
2024-07-28 09:45:33 +0200

    Fix the metric logger api (#803)
    * Train on a smaller resolution
    
    * Fix the broken loss
    
    * Use fp32 trainings
    
    * Fix the broken callback
    
    * Fix the wasp
    
    * Fix the broken metric logger

--- wasp/retinaface/monitor.py
+++ wasp/retinaface/monitor.py
@@ -23,8 +23,8 @@ class PyTorchGpuMonitorCallback(pl.Callback):
 
     def _log_average_stats(self, trainer):
         for gpu_idx, stats in enumerate(self.monitor.average_stats):
-            for key, value in stats.jsonify().items():
-                self._log_metric(f"gpu_{gpu_idx}_{key}", value)
+            for key, value in stats.items():
+                self._log_metric(trainer, f"gpu_{gpu_idx}_{key}", value)
 
     def _log_metric(self, trainer, name, value):
         if logger := trainer.logger:
