bd0f22cb4655ad8f73bee56abc3dafe61d8a65cd
kqf <q.dev.null@gmail.com>
2024-08-30 07:12:28 +0100

    Fix the resolution (#881)
    * Increase the importance of the regression loss
    
    * Use the detection loss
    
    * Add the ssd training
    
    * Addapt the ssd outputs
    
    * Add the missing ssd
    
    * Use the SSD classes
    
    * Clean the data
    
    * Clean the loss
    
    * Fix the simplified training loop
    
    * Filter out broken labels
    
    * Fix the mAP evaluation
    
    * Bump the LR and the batch size
    
    * Fix the resolution
    
    * Reduce the learning rate

--- wasp/retinaface/train.py
+++ wasp/retinaface/train.py
@@ -65,7 +65,7 @@ def main(
         priorbox=priors,
         build_optimizer=partial(
             torch.optim.Adam,
-            lr=0.004,
+            lr=0.0001,
             weight_decay=0.0001,
             # momentum=0.9,
         ),
