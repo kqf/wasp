5700f2a53707d5af90e58e5c1c6dd043c487ae63
Sasha <q.dev.null@gmail.com>
2024-09-16 07:24:54 +0100

    Fix the weights

--- wasp/retinaface/train.py
+++ wasp/retinaface/train.py
@@ -68,7 +68,7 @@ def main(
         priorbox=priors,
         build_optimizer=partial(
             torch.optim.Adam,
-            # lr=0.0001,
+            lr=0.0001,
             # weight_decay=0.0001,
             # momentum=0.9,
         ),
