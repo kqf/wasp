4de564c6bdf51f70f3b2cf6371a83a969afd50d3
kqf <q.dev.null@gmail.com>
2024-09-25 08:06:17 +0100

    Debug the artifact (#939)
    * Fix the retina pipeline
    
    * Fix the prunning
    
    * Fix the download configuration
    
    * Fix the prunning
    
    * Debug the pre-training loading
    
    * Add the artifact debugging

--- wasp/retinaface/train.py
+++ wasp/retinaface/train.py
@@ -43,7 +43,6 @@ def download_pretrained_state_dict(run_id):
 
     # Ensure the destination directory exists
     os.makedirs(dst_path, exist_ok=True)
-
     # Download the artifacts
     mlflow.artifacts.download_artifacts(
         run_id=run_id,
@@ -54,6 +53,7 @@ def download_pretrained_state_dict(run_id):
     # Construct the full path to the expected file
     file_path = os.path.join(dst_path, "best.pth")
     print(f"Checking for downloaded file at: {file_path}")
+    print(f"The file exists: {os.path.exists(file_path)=}")
 
     # Inspect the directory to see what's there
     print(f"Contents of '{dst_path}':")
@@ -61,12 +61,8 @@ def download_pretrained_state_dict(run_id):
         for file in files:
             print(f" - {file}")  # List each file found
 
-    for _, _, files in os.walk("."):
-        for file in files:
-            print(f" -> {file}")  # List each file found
-
     # Load and return the state dict
-    return torch.load(file_path)
+    return torch.load(f"./{file_path}")
 
 
 def main(
