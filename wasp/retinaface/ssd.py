e7429df14b169509f3b5483eb9f53876e18a0681
Sasha <q.dev.null@gmail.com>
2024-10-04 07:27:14 +0100

    Clean the ssd definitions

--- wasp/retinaface/ssd.py
+++ wasp/retinaface/ssd.py
@@ -1,7 +1,6 @@
 from functools import partial
 from typing import Any, Optional
 
-import numpy as np
 import torch
 import torchvision
 import tqdm
@@ -10,6 +9,7 @@ from torch.utils.data import DataLoader
 from torchvision.models.detection import SSDLite320_MobileNet_V3_Large_Weights
 from torchvision.models.detection import _utils as det_utils
 from torchvision.models.detection.anchor_utils import DefaultBoxGenerator
+from torchvision.models.detection.backbone_utils import _resnet_fpn_extractor
 from torchvision.models.detection.ssdlite import (
     SSD,
     MobileNet_V3_Large_Weights,
@@ -133,9 +133,6 @@ class RetinaNetPureHead(torch.nn.Module):
 class RetinaNetPure(torch.nn.Module):
     def __init__(self, resolution, n_classes):
         super().__init__()
-        from torchvision.models.detection.backbone_utils import (
-            _resnet_fpn_extractor,
-        )
         from torchvision.models.detection.retinanet import (
             RetinaNet_ResNet50_FPN_V2_Weights,
         )
@@ -221,68 +218,6 @@ def vis_outputs(images, boxes):
         annotations=[Annotation(bbox, ()) for bbox in boxes],
     )
     cv2.imwrite("debug.jpg", vis)
-
-
-class SSDModel(torch.nn.Module):
-    def __init__(self, model):
-        super().__init__()
-        self.model = model
-
-    def forward(self, x):
-        return (x, None) if self.model.training else (x, self.model(x))
-
-    def loss(self, batch_outs, targets):
-        batch, _ = batch_outs
-        converted = []
-        for entry in targets:
-            boxes = entry["boxes"]
-            mask = (boxes[:, 2:] > boxes[:, :2]).all(-1)
-            converted.append(
-                {k: convert(k, v, mask) for k, v in entry.items()},
-            )
-
-        if self.model.training:
-            losses: dict = self.model(batch, converted)
-        else:
-            self.model.train()
-            with torch.no_grad():
-                losses: dict = self.model(batch, converted)
-            self.model.eval()
-            # Don't do anything with losses
-            # losses = {"classification": 0, "bbox_regression": 0}
-
-        total = sum(losses.values())
-        return {
-            "loss": total,
-            "classes": losses["classification"],
-            "boxes": losses["bbox_regression"],
-        }
-
-    def prepare_outputs(self, images, out, targets, prior_box):
-        _, out = out
-        total = []
-        image_height = images.shape[2]
-        image_width = images.shape[3]
-        scale = np.tile([image_width, image_height], 2)
-        for preds, labels in zip(out, targets):
-            boxes = preds["boxes"].cpu().numpy()
-            scores = preds["scores"].cpu().numpy()
-            candidates = np.concatenate(
-                (boxes, scores.reshape(-1, 1), scores.reshape(-1, 1)),
-                axis=1,
-            )
-            candidates[:, -2] = 0
-
-            boxes_gt = labels["boxes"].cpu().numpy()
-            labels_gt = labels["labels"].cpu().numpy()
-            gts = np.zeros((boxes_gt.shape[0], 7), dtype=np.float32)
-            gts[:, :4] = boxes_gt[:, :4] * scale[None, :]
-            gts[:, 4] = np.where(labels_gt[:, -1] > 0, 0, 1)
-            total.append((candidates, gts))
-
-        # Uncomment to debug the plots
-        # vis_outputs(images, boxes)
-        return total
 
 
 def load_with_mismatch(model, pretrained_state_dict):
@@ -378,19 +313,6 @@ def ssdlite320_mobilenet_v3_large_custom(
     return load_with_mismatch(model, weights.get_state_dict(progress=progress))
 
 
-def build_model(
-    resolution=(320, 320),
-    num_classes=2,
-) -> SSDModel:
-    # Load pre-trained model
-    model = ssdlite320_mobilenet_v3_large_custom(
-        weights=SSDLite320_MobileNet_V3_Large_Weights.DEFAULT,
-        size=resolution,
-        num_classes=num_classes,
-    )
-    return SSDModel(model)
-
-
 def build_dataloader(resolution):
     env = Env()
     env.read_env()
@@ -419,7 +341,7 @@ def main(
     num_epochs=10,
     resolution=(320, 320),
 ):
-    model = build_model(resolution)
+    model = SSDPure()
     data_loader = build_dataloader(resolution)
 
     optimizer = torch.optim.SGD(
