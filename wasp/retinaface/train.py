0ac8eec0626e5a170de0e48b177882c8543d9ce0
Sasha <q.dev.null@gmail.com>
2024-10-23 07:09:51 +0200

    Decrease the learning rate

--- wasp/retinaface/train.py
+++ wasp/retinaface/train.py
@@ -87,7 +87,7 @@ def main(
         priorbox=priors,
         build_optimizer=partial(
             torch.optim.Adam,
-            lr=0.0002,
+            lr=0.00002,
             # weight_decay=0.0001,
             # momentum=0.9,
         ),
