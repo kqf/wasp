bba489d183ab43d3c8756c4a7954b6582a61c755
kqf <q.dev.null@gmail.com>
2024-10-29 07:57:07 +0100

    Increase adamw scheduler (#998)
    * Simplify adam
    
    * Fix the training
    
    * Increase the AdamW

--- wasp/retinaface/train.py
+++ wasp/retinaface/train.py
@@ -87,7 +87,7 @@ def main(
         priorbox=priors,
         build_optimizer=partial(
             torch.optim.AdamW,
-            lr=0.000002,
+            lr=0.00002,
             # weight_decay=0.0001,
             # momentum=0.9,
         ),
