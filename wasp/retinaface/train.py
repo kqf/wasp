200ca2fa5e2a2ffbe74b39fcb68ca410b8cf4bd0
kqf <q.dev.null@gmail.com>
2024-11-05 07:20:25 +0100

    Increase the amount (#1008)
    * Simplify adam
    
    * Fix the training
    
    * Increase the AdamW
    
    * Bump the adam w even more
    
    * FIx
    
    * Fix the norm and dim
    
    * REduce the amount
    
    * Don't use unstructured
    
    * Fix the norm
    
    * Fix the typo
    
    * BUmp the norm again
    
    * Add the extreme norm
    
    * Increase the amount

--- wasp/retinaface/train.py
+++ wasp/retinaface/train.py
@@ -144,7 +144,7 @@ def main(
             ModelPruning(
                 pruning_fn="ln_structured",
                 parameters_to_prune=parameters_to_prune,
-                amount=0.3,
+                amount=0.5,
                 pruning_norm=6,
                 # pruning_dim=1,
                 use_global_unstructured=False,
