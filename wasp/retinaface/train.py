f0bc2702c86941fc24559f7753bf6b81878496d9
Sasha <q.dev.null@gmail.com>
2024-11-07 07:53:26 +0100

    Decrease the prunning norm

--- wasp/retinaface/train.py
+++ wasp/retinaface/train.py
@@ -144,8 +144,8 @@ def main(
             ModelPruning(
                 pruning_fn="ln_structured",
                 parameters_to_prune=parameters_to_prune,
-                amount=0.6,
-                pruning_norm=8,
+                amount=0.5,
+                pruning_norm=7,
                 # pruning_dim=1,
                 use_global_unstructured=False,
                 # use_global_unstructured=False,
