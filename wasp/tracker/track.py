20566f64d9276cfdfd36629d50e4a58b0970a2cb
Sasha <q.dev.null@gmail.com>
2024-12-02 08:12:37 +0100

    Add the old overlay

--- wasp/tracker/track.py
+++ wasp/tracker/track.py
@@ -68,6 +68,19 @@ SEGMENTS = {
 }
 
 
+def overlay_bbox_on_frame_simple(frame, bbox, max_size=256, o_x=40):
+    x, y, w, h = bbox
+    roi = frame[y : y + h, x : x + w]
+    scale = min(max_size / w, max_size / h)
+    new_width = int(w * scale)
+    new_height = int(h * scale)
+    resized_roi = cv2.resize(roi, (new_width, new_height))
+    frame_height, frame_width = frame.shape[:2]
+    o_y = frame_height - new_height - 10
+    frame[o_y : o_y + new_height, o_x : o_x + new_width] = resized_roi
+    return frame
+
+
 def overlay_bbox_on_frame(frame, bbox, max_size=256, o_x=40):
     x, y, w, h = bbox
     center_x, center_y = x + w // 2, y + h // 2
