ec7991b4bee74a15ca5ce7d19fb4f7348081e46c
kqf <q.dev.null@gmail.com>
2024-12-21 09:49:39 +0100

    Finalize sift based object detector (#1096)
    * Add the capture
    
    * Fix the tracker
    
    * Fix the broken tracking code
    
    * Add the sift tracker
    
    * Fix the warnings
    
    * Fix the sift tracker
    
    * Finalize the sift-based object detector

--- wasp/tracker/tracker.py
+++ wasp/tracker/tracker.py
@@ -11,6 +11,10 @@ def add_weighted(a, b, alpha):
     return (alpha * r + (1 - alpha) * b).astype(np.uint8)
 
 
+def add_weighted_(a, b, alpha):
+    return alpha * a + (1 - alpha) * b
+
+
 def calculate_search_area(frame, last_position, n=3):
     x, y, w, h = last_position
     search_w = w * n
@@ -32,19 +36,13 @@ def calculate_search_area(frame, last_position, n=3):
 
 
 def select_good_matches(
-    descriptors_t,
-    descriptors_frame,
+    descriptors1,
+    descriptors2,
     ratio_threshold=0.75,
 ):
     bf_matcher = cv2.BFMatcher()
-    matches = bf_matcher.knnMatch(descriptors_t, descriptors_frame, k=2)
-
-    good_matches = []
-    for m, n in matches:
-        # if m.distance < ratio_threshold * n.distance:
-        good_matches.append(m)
-
-    return good_matches
+    matches = bf_matcher.knnMatch(descriptors1, descriptors2, k=2)
+    return [m for m, n in matches]
 
 
 def calculate_shift(src_pts, dst_pts):
@@ -60,7 +58,7 @@ def calculate_scale(src_pts, dst_pts):
     src_distances = np.linalg.norm(src_pts - src_centroid, axis=1)
     dst_distances = np.linalg.norm(dst_pts - dst_centroid, axis=1)
     scale = np.median(dst_distances) / max(np.median(src_distances), 1e-5)
-    return scale * 0 + 1.0
+    return scale * 0.0 + 1
 
 
 def ensure_within_frame(frame, new_x, new_y, new_w, new_h):
@@ -73,37 +71,37 @@ def ensure_within_frame(frame, new_x, new_y, new_w, new_h):
 
 
 def plot_detected_features(
-    frame, keypoints_t, keypoints_frame, good_matches, search_x, search_y
+    frame,
+    keypoints,
 ):
-    for point in keypoints_frame:
-        point.pt = point.pt[0] + search_x, point.pt[1] + search_y
-
-    for point in keypoints_t:
-        point.pt = point.pt[0] + search_x, point.pt[1] + search_y
-
-    # Draw the keypoints of the template
     frame_with_keypoints = cv2.drawKeypoints(
         frame,
-        keypoints_frame,
+        keypoints,
         None,
         flags=cv2.DrawMatchesFlags_DRAW_RICH_KEYPOINTS,
     )
     cv2.imshow("template", frame_with_keypoints)
 
-    # Draw the matches on the frame
-    if len(good_matches) > 0:
-        img_matches = cv2.drawMatches(
-            frame,
-            keypoints_t,
-            frame_with_keypoints,
-            keypoints_frame,
-            good_matches,
-            None,
-            flags=cv2.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS,
-        )
-        cv2.imshow("keypts", img_matches)
+
+def compare_frames(
+    frame1,
+    keypoints1,
+    frame2,
+    keypoints2,
+    good_matches,
+):
+    img_matches = cv2.drawMatches(
+        frame1,
+        keypoints1,
+        frame2,
+        keypoints2,
+        good_matches,
+        None,
+        flags=cv2.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS,
+    )
+    cv2.imshow("keypts", img_matches)
     cv2.waitKey()
-    return frame_with_keypoints
+    return img_matches
 
 
 class SIFTTracker:
@@ -120,24 +118,35 @@ class SIFTTracker:
         self.template = frame[y : y + h, x : x + w]
         self.last_position = (x, y, w, h)
         self.initialized = True
+        self.prev_frame = frame
+
+    def _detect(self, image, coords) -> tuple:
+        kpts, descriptors = self.sift.detectAndCompute(image, None)
+        x, y, *_ = coords
+        for point in kpts:
+            point.pt = point.pt[0] + x, point.pt[1] + y
+        return kpts, descriptors
 
     def update(self, frame):
         if not self.initialized:
             raise RuntimeError("Tracker not initialized. Call `init` first.")
 
-        keypoints_t, descriptors_t = self.sift.detectAndCompute(
+        keypoints_t, descriptors_t = self._detect(
             self.template,
-            None,
+            self.last_position,
         )
 
         # Calculate the search area
-        search_area, (sx, sy, sw, sh) = calculate_search_area(
-            frame, self.last_position, self.n
+        search_area, sbox = calculate_search_area(
+            frame,
+            self.last_position,
+            self.n,
         )
 
         # Detect keypoints and descriptors in the search area
-        keypoints_frame, descriptors_frame = self.sift.detectAndCompute(
-            search_area, None
+        keypoints_frame, descriptors_frame = self._detect(
+            search_area,
+            sbox,
         )
 
         if descriptors_frame is None or len(keypoints_frame) == 0:
@@ -147,13 +156,12 @@ class SIFTTracker:
         # Select good matches
         good_matches = select_good_matches(descriptors_t, descriptors_frame)
 
-        plot_detected_features(
-            frame,
+        compare_frames(
+            self.prev_frame,
             keypoints_t,
+            frame,
             keypoints_frame,
-            good_matches,
-            sx,
-            sy,
+            good_matches=good_matches,
         )
 
         if len(good_matches) < self.min_matches:
@@ -164,22 +172,21 @@ class SIFTTracker:
         src_pts = np.float32(
             [keypoints_t[m.queryIdx].pt for m in good_matches]
         ).reshape(-1, 2)
+
         dst_pts = np.float32(
             [keypoints_frame[m.trainIdx].pt for m in good_matches]
         ).reshape(-1, 2)
 
-        # Adjust the source points to map them to the full frame
-        src_pts -= np.array([sx, sy])
-
         # Calculate the displacement and scale
         dx, dy = calculate_shift(src_pts, dst_pts)
-        scale = calculate_scale(src_pts, dst_pts)
-
+        # scale = calculate_scale(src_pts, dst_pts)
+        x, y, w, h = self.last_position
+        *_, w1, h1 = max_bounding_box(dst_pts)
         # Update the bounding box position and size
-        new_x = int(dx)
-        new_y = int(dy)
-        new_w = int(sw * scale)  # Scale the width
-        new_h = int(sh * scale)  # Scale the height
+        new_x = int(x + dx)
+        new_y = int(y + dy)
+        new_w = int(add_weighted_(w, w1, 0.99))  # Scale the width
+        new_h = int(add_weighted_(h, h1, 0.99))  # Scale the height
 
         # Ensure the new bounding box is within frame bounds
         new_x, new_y, new_w, new_h = ensure_within_frame(
@@ -188,11 +195,11 @@ class SIFTTracker:
 
         # Update template with blending
         new_template = frame[new_y : new_y + new_h, new_x : new_x + new_w]
-        alpha = 1
-        self.template = add_weighted(self.template, new_template, alpha)
-
-        # Update the last position
+        # alpha = 0
+        # self.template = add_weighted(self.template, new_template, alpha)
+        self.template = new_template
         self.last_position = (new_x, new_y, new_w, new_h)
+        self.prev_frame = frame
 
         return True, self.last_position
 
