9fee493747dde1a623c4e9b8e3690a6537425ef0
kqf <q.dev.null@gmail.com>
2024-12-26 08:27:18 +0100

    Visualize the features (#1107)
    * Add plotting; rename the functions
    
    * Update the features properly
    
    * Plot the frames

--- wasp/tracker/custom/ofs.py
+++ wasp/tracker/custom/ofs.py
@@ -10,7 +10,7 @@ class OpticalFLowSimplified:
     def __init__(self):
         self.bbox = None
         self.previous_frame = None
-        self.previous_points = np.array([], dtype=np.float32).reshape(0, 2)
+        self.features = np.array([], dtype=np.float32).reshape(0, 2)
         self.object_lost = False
 
     def init(self, frame: np.ndarray, bbox: XYWH) -> bool:
@@ -18,23 +18,23 @@ class OpticalFLowSimplified:
         self.previous_frame = frame.copy()
         mask = frame[bbox[1] : bbox[1] + bbox[3], bbox[0] : bbox[0] + bbox[2]]
 
-        self.previous_points = cv2.goodFeaturesToTrack(mask, 100, 0.3, 7)
-        if self.previous_points is None:
-            self.previous_points = np.array([], dtype=np.float32).reshape(0, 2)
+        self.features = cv2.goodFeaturesToTrack(mask, 100, 0.3, 7)
+        if self.features is None:
+            self.features = np.array([], dtype=np.float32).reshape(0, 2)
             self.object_lost = True
             return False
 
-        self.previous_points = self.previous_points.reshape(-1, 2)
-        self.previous_points[:, 0] += bbox[0]
-        self.previous_points[:, 1] += bbox[1]
+        self.features = self.features.reshape(-1, 2)
+        self.features[:, 0] += bbox[0]
+        self.features[:, 1] += bbox[1]
         self.object_lost = False
         return True
 
     def displacement(self, frame: np.ndarray) -> Tuple[float, float]:
-        if len(self.previous_points) == 0:
+        if len(self.features) == 0:
             return 0.0, 0.0
 
-        mean_point = np.mean(self.previous_points, axis=0)
+        mean_point = np.mean(self.features, axis=0)
         center = np.array([frame.shape[1] / 2.0, frame.shape[0] / 2.0])
 
         displacement = mean_point - center
@@ -50,24 +50,45 @@ class OpticalFLowSimplified:
         new_points, status, _ = cv2.calcOpticalFlowPyrLK(
             self.previous_frame,
             frame,
-            self.previous_points,
+            self.features,
             None,
         )
 
         # Filter points based on status
         valid_new_points = new_points[status.flatten() == 1]
-        valid_old_points = self.previous_points[status.flatten() == 1]
+        valid_old_points = self.features[status.flatten() == 1]
 
-        if len(valid_new_points) < len(self.previous_points) * 0.5:
+        if len(valid_new_points) < len(self.features) * 0.5:
             print("Error: Lost too many features, object is lost")
             self.object_lost = True
             return False, self.bbox
 
-        self.previous_frame = frame.copy()
-        self.previous_points = valid_new_points
-
         dx, dy = np.mean(valid_new_points - valid_old_points, axis=0)
         x, y, w, h = self.bbox
         self.bbox = (int(x + dx), int(y + dy), w, h)
+        self.last_features = self.features + (dx, dy)
+        self.features = valid_new_points
+        self.previous_frame = frame.copy()
 
         return True, self.bbox
+
+    def plot(self, frame: np.ndarray) -> np.ndarray:
+        for feature in self.last_features.reshape(-1, 2):
+            x, y = int(feature[0]), int(feature[1])
+            frame = cv2.circle(
+                frame,
+                (x, y),
+                radius=2,
+                color=(0, 0, 255),
+                thickness=-1,
+            )
+        for feature in self.features.reshape(-1, 2):
+            x, y = int(feature[0]), int(feature[1])
+            frame = cv2.circle(
+                frame,
+                (x, y),
+                radius=5,
+                color=(0, 255, 0),
+                thickness=-1,
+            )
+        return frame
