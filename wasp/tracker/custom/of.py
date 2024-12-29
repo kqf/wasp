Fix the scaling (#1113)
* Add plotting; rename the functions

* Update the features properly

* Plot the frames

* Update the optical flow

* Remove the redundant code

* Switch back to of

* Fix the bbox calculation

* Add the average distance

* Fix the scaling

wasp/tracker/custom/of.py
@@ -148,14 +148,14 @@ def make_sure_includes_all(
 
 def calculate_scale_change(old_features, new_features):
     old_center = np.mean(old_features, axis=0)
-    old_distances = np.linalg.norm(old_features - old_center, axis=1)
+    old_distances = np.linalg.norm(old_features - old_center, axis=0)
 
     new_center = np.mean(new_features, axis=0)
-    new_distances = np.linalg.norm(new_features - new_center, axis=1)
-    scale_ratio = np.mean(new_distances) / np.mean(old_distances)
-    return scale_ratio, scale_ratio
+    new_distances = np.linalg.norm(new_features - new_center, axis=0)
+    scale_ratio = new_distances / old_distances
+    return np.nan_to_num(scale_ratio, nan=1.0)
 
-
+  
 @dataclass
 class OpticalFLowTracker:
     bbox: XYWH = (0, 0, 0, 0)
@@ -206,15 +206,10 @@ class OpticalFLowTracker:
         y_new -= h // 2
 
         # Calculate scale change
-        # TODO: To be implemented:
-        # the lagorithm is as follows:
-        # 1. Calculate center in x and y
-        # 2. Calculate average distance from the center
-        # 3. Calcualte the average distance to the center for new features
-        # 4. Compute the ratio between the distances
-        scale_h, scale_w = calculate_scale_change(old_features, new_features)
-        h_new = h * scale_h
-        w_new = w * scale_w
+        scale_w, scale_h = calculate_scale_change(old_features, new_features)
+        alpha = 0.1
+        h_new = alpha * h * scale_h + (1 - alpha) * h
+        w_new = alpha * w * scale_w + (1 - alpha) * w
 
         # Update tracker state
         self.bbox = (int(x_new), int(y_new), int(w_new), int(h_new))