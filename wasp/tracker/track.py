342fd017e73bfabd8b9bb5611998c0e97c51cdb3
Sasha <q.dev.null@gmail.com>
2025-01-06 06:42:18 +0000

    Add optical flow simplified

--- wasp/tracker/track.py
+++ wasp/tracker/track.py
@@ -68,6 +68,7 @@ TRACKERS = {
     "cv2.legacy.TrackerMF_create": cv2.legacy.TrackerMF_create,
     "cv2.legacy.TrackerTLD_create": cv2.legacy.TrackerTLD_create,
     "OF": OpticalFLowTracker,
+    "OFS": OpticalFLowSimplified,
 }
 
 
