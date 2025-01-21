e1aaa552f6419dae8503eb56b3668cfc654521f3
Sasha <q.dev.null@gmail.com>
2025-01-21 06:35:44 +0000

    Fix the CLI

--- wasp/cli.py
+++ wasp/cli.py
@@ -9,6 +9,15 @@ def wasp():
 
 @wasp.command()
 def track():
+    """Start the experiments with tracker"""
     from wasp.tracker.main import main  # noqa
+
+    main()
+
+
+@wasp.command()
+def train():
+    """Train the detector"""
+    from wasp.retinaface.train import main  # noqa
 
     main()
