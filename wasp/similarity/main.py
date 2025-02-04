1969c6de72a6f19da7157c81f6e17e4cfbb940a4
Sasha <q.dev.null@gmail.com>
2025-02-04 06:49:15 +0000

    Add the box head

--- wasp/similarity/main.py
+++ wasp/similarity/main.py
@@ -7,6 +7,23 @@ from torchvision import models
 from wasp.retinaface.priors import priorbox
 
 
+class BboxHead(torch.nn.Module):
+    def __init__(self, in_channels: int = 512, num_anchors: int = 3):
+        super().__init__()
+        self.conv1x1 = torch.nn.Conv2d(
+            in_channels,
+            num_anchors * 4,
+            kernel_size=(1, 1),
+            stride=(1, 1),
+            padding=0,
+        )
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        out = self.conv1x1(x)
+        out = out.permute(0, 2, 3, 1).contiguous()
+        return out.view(out.shape[0], -1, 4)
+
+
 class ResNet50Backbone(torch.nn.Module):
     def __init__(self):
         super().__init__()
