59f6d68d6571d882fe582dda9a239861db066f7b
Sasha <q.dev.null@gmail.com>
2025-02-17 09:56:36 +0000

    Add both visualizations

--- wasp/triangulation/mp.py
+++ wasp/triangulation/mp.py
@@ -2,6 +2,16 @@ import cv2
 import mediapipe as mp
 
 
+def draw_landmarks(image, landmarks):
+    for landmark in landmarks:
+        x, y = int(landmark.x * image.shape[1]), int(
+            landmark.y * image.shape[0]
+        )
+        cv2.circle(
+            image, (x, y), 2, (255, 0, 0), -1
+        )  # Mark face landmarks in blue
+
+
 def draw_iris_landmarks(image, landmarks):
     iris_indices = [
         468,
@@ -19,9 +29,6 @@ def draw_iris_landmarks(image, landmarks):
             landmark.y * image.shape[0]
         )
         cv2.circle(image, (x, y), 2, (0, 255, 0), -1)  # Mark iris in green
-    cv2.imshow("Iris Landmarks", image)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
 
 
 def main():
@@ -36,7 +43,16 @@ def main():
     face_mesh_results = face_mesh.process(image_rgb)
     if face_mesh_results.multi_face_landmarks:
         for face_landmarks in face_mesh_results.multi_face_landmarks:
-            draw_iris_landmarks(image, face_landmarks.landmark)
+            draw_landmarks(
+                image, face_landmarks.landmark
+            )  # Draw facial landmarks
+            draw_iris_landmarks(
+                image, face_landmarks.landmark
+            )  # Draw iris landmarks
+
+    cv2.imshow("Face and Iris Landmarks", image)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
 
 
 if __name__ == "__main__":
