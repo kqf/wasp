46924a42f14f5a6e0792259c5b2dd5b9840e8aca
kqf <q.dev.null@gmail.com>
2025-02-20 06:23:17 +0000

    Fix the cropping (#1216)
    * Remove the keypoints example
    
    * Fix the cropping

--- wasp/tracker/custom/cropped.py
+++ wasp/tracker/custom/cropped.py
@@ -1,13 +1,28 @@
 import cv2
 
 
-def crop_region(frame, bbox, pad):
+def crop_region(frame, bbox, margin):
     x, y, w, h = map(int, bbox)
-    x1, y1 = max(0, x - pad), max(0, y - pad)
-    x2, y2 = min(frame.shape[1], x + w + pad), min(frame.shape[0], y + h + pad)
+
+    x1, y1 = x - margin, y - margin
+    x2, y2 = x + w + margin, y + h + margin
+
+    if x1 < 0:
+        x2 = min(frame.shape[1], x2 + abs(x1))  # Shift crop right
+        x1 = 0
+    if y1 < 0:
+        y2 = min(frame.shape[0], y2 + abs(y1))  # Shift crop down
+        y1 = 0
+    if x2 > frame.shape[1]:
+        x1 = max(0, x1 - (x2 - frame.shape[1]))  # Shift crop left
+        x2 = frame.shape[1]
+    if y2 > frame.shape[0]:
+        y1 = max(0, y1 - (y2 - frame.shape[0]))  # Shift crop up
+        y2 = frame.shape[0]
+
     cropped = frame[y1:y2, x1:x2]
     new_bbox = (x - x1, y - y1, w, h)
-    return cropped, new_bbox, x1, y1  # Also return the crop offsets
+    return cropped, new_bbox, x1, y1
 
 
 def map_to_original(crop_x, crop_y, new_bbox):
