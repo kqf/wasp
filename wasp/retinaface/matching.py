212c44c010a7be416386f89f78de735b562983f0
Sasha <q.dev.null@gmail.com>
2025-04-13 12:32:08 +0100

    Remove matching

--- wasp/retinaface/matching.py
+++ wasp/retinaface/matching.py
@@ -32,44 +32,6 @@ def match(
     boxes: torch.Tensor,  # [n_obj, 4]
     priors: torch.Tensor,  # [n_anchors, 4]
     threshold: float,
-) -> tuple[torch.Tensor, torch.Tensor] | tuple[None, None]:
-    overlaps = iou(boxes[:, None], point_form(priors))  # [n_obj, n_anchors]
-
-    # (Bipartite Matching)
-    best_prior_overlap, best_prior_idx = overlaps.max(
-        1, keepdim=True
-    )  # [n_obj, 1], [n_obj, 1]
-    valid_gt_idx = best_prior_overlap[:, 0] >= 0.2  # [n_obj]
-
-    if valid_gt_idx.sum() == 0:
-        return None, None
-
-    best_truth_overlap, best_truth_idx = overlaps.max(
-        0, keepdim=True
-    )  # [1, n_anchors], [1, n_anchors]
-    best_truth_overlap = best_truth_overlap.squeeze(0)  # [n_anchors]
-    best_prior_idx = best_prior_idx.squeeze(1)  # [n_obj]
-    best_truth_overlap.index_fill_(0, best_prior_idx[valid_gt_idx], 2)
-
-    best_truth_idx = best_truth_idx.squeeze(0)  # [n_anchors]
-
-    # Use arange instead of for loop
-    best_truth_idx[best_prior_idx] = torch.arange(
-        best_prior_idx.shape[0],
-        device=best_prior_idx.device,
-    )
-
-    labels_matched = labels[best_truth_idx].clone()  # [n_anchors]
-    labels_matched[best_truth_overlap < threshold] = 0
-
-    return best_truth_idx, labels_matched
-
-
-def match2(
-    labels: torch.Tensor,  # [n_obj]
-    boxes: torch.Tensor,  # [n_obj, 4]
-    priors: torch.Tensor,  # [n_anchors, 4]
-    threshold: float,
 ) -> torch.Tensor:  # returns a tensor of shape [n_anchors, n_obj]
     n_anchors = priors.shape[0]
     n_obj = boxes.shape[0]
